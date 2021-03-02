448d1b70d26ea736835977dce4e997daf6fa4fc5
Vera-NL <78375434+Vera-NL@users.noreply.github.com>
2021-03-02 20:13:01 +0100

    Added a search engine

--- src/app.js
+++ src/app.js
@@ -15,22 +15,30 @@ let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector("#wind");
 let dateElement = document.querySelector("#date");
 let iconElement = document.querySelector("#icon");
+let icon = response.data.weather[0].icon;
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity;
 windElement.innerHTML = Math.round(response.data.wind.speed);
 dateElement.innerHTML = formatDate(response.data.dt * 1000);
-let icon = response.data.weather[0].icon;
 iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${icon}@2x.png`);
 iconElement.setAttribute("alt", response.data.weather[0].description)
 }
 
-//http://openweathermap.org/img/wn/01d@2x.png
+function search(city) {
+    let apiKey = "ad1c3c6d8734a6f724e8c027e1f76c71";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showTemperature);
+}
 
-let apiKey = "ad1c3c6d8734a6f724e8c027e1f76c71";
-let city = "Istanbul";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
 
-axios.get(apiUrl).then(showTemperature);
+search("Amsterdam");
 
+let form = document.querySelector("#seach-form");
+form.addEventListener("submit", handleSubmit);
