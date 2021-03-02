e07a2e38af82ee39e15a255a2d78e55ef1eea248
Vera-NL <78375434+Vera-NL@users.noreply.github.com>
2021-03-02 13:16:30 +0100

    Showing current temperature (JS)

---
+++ src/app.js
@@ -0,0 +1,19 @@
+function showTemperature(response) {
+    console.log(response);
+let temperatureElement = document.querySelector("#temperature");
+let cityElement = document.querySelector("#city");
+let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windElement = document.querySelector("#wind");
+temperatureElement.innerHTML = Math.round(response.data.main.temp);
+cityElement.innerHTML = response.data.name;
+descriptionElement.innerHTML = response.data.weather[0].description;
+humidityElement.innerHTML = response.data.main.humidity;
+windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "ad1c3c6d8734a6f724e8c027e1f76c71";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(showTemperature);
+
